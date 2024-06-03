0d6db2956da9d9f86f3020275629399b28cd07d3
Musrial <105841107221@student.unismuh.ac.id>
2024-06-03 21:31:20 +0800

    Tugas 2

--- App.js
+++ App.js
@@ -1,29 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
+import { StyleSheet,Text, View } from "react-native";
+import React from "react";
 const App = () => {
-  return(
+  return (
+
     <View style = {{
-      flex: 1,
+      flex: 1, 
+      flexDirection: "row",
+      alignItems: "flex-end",
+      justifyContent: "center",
     }}>
-     <View style = {{
-      flex: 1,flexDirection : 'row', backgroundColor : 'white'}}>
       <View style = {{
-      flex: 1, backgroundColor : 'yellow'}}>
-    </View>
-    <View style = {{
-      flex: 1, backgroundColor : 'green'}}>
-    </View>
-    </View>
-     <View style = {{
-      flex: 1, backgroundColor : 'red'}}>
-    </View>
-    <View style = {{
-      flex: 1, backgroundColor : 'blue'}}>
+        backgroundColor:"purple", 
+        width: 125, 
+        height: 60,
+        justifyContent: "center",
+        borderRadius: 20,
+        marginRight: 20,
+        marginBottom:20,
+      }}>
+        <Text style = {{
+          fontSize: 20, color: "white",
+          textAlign: "center",
+          fontWeight: "bold",
+        }}>Masuk</Text>
+      </View>
+
+      <View style = {{
+        backgroundColor: "purple", 
+        width:125, 
+        height: 60,
+        justifyContent: "center",
+        borderRadius: 20,
+        marginLeft: 20,
+        marginBottom: 20,
+      }}>
+        <Text style = {{
+          fontSize: 20, color: "white",
+          textAlign: "center",
+          fontWeight: "bold",
+        }}>Daftar</Text>
       </View>
     </View>
-    
-    
-  )
-}
-export default App
+  );
+};
+export default App;
