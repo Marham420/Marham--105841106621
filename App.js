db6938ed45d41c00b9c67d3e0cbab5c1410b857f
Musrial <105841107221@student.unismuh.ac.id>
2024-07-31 02:38:01 +0800

    Text Input

--- App.js
+++ App.js
@@ -1,64 +1,39 @@
-import { StyleSheet,Text, View, Image } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native-web";
+import React from "react" ;
+const TextInputCustom = ({ name, color}) => {
+  return (
+    <TextInput placeholder={` ${name}`} style={styles.input(color)} />
+  );
+}; 
+
 const App = () => {
   return (
-
-    <View style = {{ 
-      flex: 1,
-      justifyContent:"center",
-      alignItems: "center",
-    }}>
-      <Text style = {{
-        fontSize: 30,
-        fontWeight: "bold",
-        marginBottom: 20,
-        position: "center",
-        top: 160,
-      }}>Selamat Datang
-      </Text>
-      <Image
-        source = {require('./assets/Marham Shop.png')}
-        style = {{width: 200, height: 200, marginBottom: 20, top :200, borderRadius: 50, }}
-        />
-    <View style = {{
-      flex: 1, 
-      flexDirection: "row",
-      alignItems: "flex-end",
-      justifyContent: "center",
-    }}>
-      <View style = {{
-        backgroundColor:"purple", 
-        width: 125, 
-        height: 60,
-        justifyContent: "center",
-        borderRadius: 20,
-        marginRight: 20,
-        marginBottom:20,
-      }}>
-        <Text style = {{
-          fontSize: 20, color: "white",
-          textAlign: "center",
-          fontWeight: "bold",
-        }}>Masuk</Text>
+    <View style={styles.container}>
+      <TextInputCustom name = "User" color = "red"/>
+      <TextInputCustom name = "Email" color = "red"/>
+      <TextInputCustom name = "Password" color = "red"/>
       </View>
-
-      <View style = {{
-        backgroundColor: "purple", 
-        width:125, 
-        height: 60,
-        justifyContent: "center",
-        borderRadius: 20,
-        marginLeft: 20,
-        marginBottom: 20,
-      }}>
-        <Text style = {{
-          fontSize: 20, color: "white",
-          textAlign: "center",
-          fontWeight: "bold",
-        }}>Daftar</Text>
-      </View>
-    </View>
-    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, justifyContent: "center", alignItems: "center",
+  },
+
+  input: (color) => ({
+    height: 50,
+    width: 300,
+    backgroundColor: "black",
+    marginBottom: 10,
+    borderRadius: 10,
+    borderWidth:1,
+    paddingLeft: 10,
+    color: color,
+  }),
+});
+
+
+
+
 export default App;
